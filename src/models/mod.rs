cbbf096a4b4087fb4e50646b78260b3cc594fd4c
zhoujinghui <neo350942>
2022-08-16 11:45:20 +0800

    feat(cls_head): impl cls linear head for cls_heads and test

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,2 +1,3 @@
 pub mod backbones;
-
+pub mod necks;
+pub mod cls_heads;
---
+++ tests/models_cls_heads_test.rs
@@ -0,0 +1,18 @@
+extern crate river;
+use river::{
+    Device,
+    nn,
+    Tensor,
+    kind,
+    nn::ModuleT,
+};
+#[test]
+fn test_linear_head(){
+    const IN: i64 = 512;
+    const OUT:i64 = 2;
+    use river::models::cls_heads::linear_head;
+    let vs = nn::VarStore::new(Device::cuda_if_available());
+    let net = linear_head::LinearClsHead::new(&vs.root(), IN, OUT);
+    let t = Tensor::zeros(&[1,IN], kind::FLOAT_CPU).to_device(vs.device());
+    let o_ = net.forward_t(&t, true);
+}
