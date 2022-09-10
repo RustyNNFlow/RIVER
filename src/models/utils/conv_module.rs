f011b741f9d9574abac231ad7dc4718e70224b45
zhoujinghui <neo350942>
2022-09-10 17:13:12 +0800

    feat(models): add activation option to conv_module

--- src/models/utils/conv_module.rs
+++ src/models/utils/conv_module.rs
@@ -2,6 +2,7 @@ use crate::{
     nn,
     nn::SequentialT,
     models::utils::conv_2d::conv2d,
+    Tensor,
 };
 
 
@@ -12,9 +13,13 @@ pub fn conv_module(
     ksize:i64,
     stride:i64,
     padding:i64,
-)
-    ->SequentialT {
-    nn::seq_t()
-        .add(conv2d(p / "conv", c_in, c_out, ksize, padding, stride))
-        .add(nn::batch_norm2d(p / "bn", c_out, Default::default()))
-}
+    activation:bool,
+) ->SequentialT {
+        let mut layer = nn::seq_t()
+            .add(conv2d(p / "conv", c_in, c_out, ksize, padding, stride))
+            .add(nn::batch_norm2d(p / "bn", c_out, Default::default()));
+        if activation{
+            layer=layer.add(nn::relu());
+        }
+        layer
+    }
